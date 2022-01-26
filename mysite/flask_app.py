72d0ec7e394b16b2c2fa19bc3c5310f853c1bc57
Maksim Lozovskii <46424539+MaksimLozovskii@users.noreply.github.com>
2022-01-26 04:12:59 +0000

    :fire: "ADD new routes and links to flaskApp"

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -7,6 +7,7 @@ path = "/home/HAIRYCACTUS/mysite/assets/scheduled/config.json"
 app = Flask(__name__)
 
 # Define flask routes
+<<<<<<< HEAD
 @app.route('/', methods=["GET", "POST"])
 def serve_home():
 
@@ -15,6 +16,56 @@ def serve_home():
         return redirect(url_for("serve_success"))
     else:
         return render_template("index.html")
+=======
+@app.route('/')
+def serve_homePage():
+    return render_template("index.html")
+
+@app.route('/userOne', methods=["GET", "POST"])
+def serve_userOne():
+
+    if request.method == "POST":
+        change("userOne")
+        return redirect(url_for("serve_success"))
+    else:
+        return render_template("responsePage.html")
+
+@app.route('/userTwo', methods=["GET", "POST"])
+def serve_userTwo():
+
+    if request.method == "POST":
+        change("userTwo")
+        return redirect(url_for("serve_success"))
+    else:
+        return render_template("responsePage.html")
+
+@app.route('/userThree', methods=["GET", "POST"])
+def serve_userThree():
+
+    if request.method == "POST":
+        change("userThree")
+        return redirect(url_for("serve_success"))
+    else:
+        return render_template("responsePage.html")
+
+@app.route('/userFour', methods=["GET", "POST"])
+def serve_userFour():
+
+    if request.method == "POST":
+        change("userFour")
+        return redirect(url_for("serve_success"))
+    else:
+        return render_template("responsePage.html")
+
+@app.route('/userFive', methods=["GET", "POST"])
+def serve_userFive():
+
+    if request.method == "POST":
+        change("userFive")
+        return redirect(url_for("serve_success"))
+    else:
+        return render_template("responsePage.html")
+>>>>>>> 662d463 (ADD new links and routes for flask_app.py :fire:)
 
 @app.route('/success')
 def serve_success():
--- mysite/statusSetter.py
+++ mysite/statusSetter.py
@@ -2,11 +2,19 @@ import json
 
 path = "/home/HAIRYCACTUS/mysite/assets/scheduled/config.json"
 
+<<<<<<< HEAD
 def change():
     fr = open(path, "r")
     data_object = json.load(fr)
     fr.close()
     data_object["userStatus"] = "answered"
+=======
+def change(user):
+    fr = open(path, "r")
+    data_object = json.load(fr)
+    fr.close()
+    data_object[user] = "answered"
+>>>>>>> 662d463 (ADD new links and routes for flask_app.py :fire:)
 
     with open(path, "w") as fw:
         json.dump(data_object, fw)
