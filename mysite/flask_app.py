255beea1843adf7c5591aef01871ad884983cefa
Maksim Lozovskii <46424539+MaksimLozovskii@users.noreply.github.com>
2022-01-26 11:37:18 +0000

    :fire: FIX commit errors

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -7,16 +7,6 @@ path = "/home/HAIRYCACTUS/mysite/assets/scheduled/config.json"
 app = Flask(__name__)
 
 # Define flask routes
-<<<<<<< HEAD
-@app.route('/', methods=["GET", "POST"])
-def serve_home():
-
-    if request.method == "POST":
-        change()
-        return redirect(url_for("serve_success"))
-    else:
-        return render_template("index.html")
-=======
 @app.route('/')
 def serve_homePage():
     return render_template("index.html")
@@ -65,7 +55,6 @@ def serve_userFive():
         return redirect(url_for("serve_success"))
     else:
         return render_template("responsePage.html")
->>>>>>> 662d463 (ADD new links and routes for flask_app.py :fire:)
 
 @app.route('/success')
 def serve_success():
